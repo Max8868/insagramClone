c7093d48f8625ad4f7a154a256024961ea27143c
Taciano <maximotco@hotmail.com>
2023-01-04 19:45:01 -0300

    18. Finishing Up Profile UI

--- InstagranClone/View/Profile/ProfileCell.swift
+++ InstagranClone/View/Profile/ProfileCell.swift
@@ -11,13 +11,21 @@ import UIKit
 class ProfileCell: UICollectionViewCell {
     
     //MARK: - Properties
-    
+    private let postImage: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "venom-7"))
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .lightGray
+        
+        addSubview(postImage)
+        postImage.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
--- InstagranClone/View/Profile/ProfileHeader.swift
+++ InstagranClone/View/Profile/ProfileHeader.swift
@@ -11,17 +11,131 @@ import UIKit
 class ProfileHeader: UICollectionReusableView {
     
     //MARK: - Properties
+    private let profileImage: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "venom-7"))
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Eddie Brock"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Profile", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleEditProfileFollowButton(sender:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var postLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = atrributedStackText(value: 5, label: "posts")
+        return label
+    }()
+    
+    private lazy var followersLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = atrributedStackText(value: 1, label: "followers")
+        return label
+    }()
+    
+    private lazy var followingLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = atrributedStackText(value: 1, label: "following")
+        return label
+    }()
+    
+    lazy var gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "grid"), for: .normal)
+        return button
+    }()
+    
+    lazy var listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "list"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    lazy var bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "ribbon"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
     
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemPink
+        
+        backgroundColor = .white
+        addSubview(profileImage)
+        profileImage.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
+        profileImage.setDimensions(height: 80, width: 80)
+        profileImage.layer.cornerRadius = 80 / 2
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: profileImage.bottomAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 15, paddingLeft: 24, paddingRight: 24)
+        
+        let stack = UIStackView(arrangedSubviews: [postLabel, followersLabel, followingLabel])
+        stack.distribution = .fillEqually
+        addSubview(stack)
+        stack.centerY(inView: profileImage)
+        stack.anchor(left: profileImage.rightAnchor, right: rightAnchor, paddingLeft: 12, paddingRight: 12, height: 50)
+        
+        let topDivider = UIView()
+        topDivider.backgroundColor = .lightGray
+        
+        let bottomDivider = UIView()
+        bottomDivider.backgroundColor = .lightGray
+        
+        let bottomStack = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        bottomStack.distribution = .fillEqually
+        
+        addSubview(bottomStack)
+        addSubview(topDivider)
+        addSubview(bottomDivider)
+        
+        bottomStack.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
+        topDivider.anchor(top: bottomStack.topAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+        bottomDivider.anchor(top: bottomStack.bottomAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: - Actions
+    @objc private func handleEditProfileFollowButton(sender: UIButton) {
+        print("DEBUG: Handle edit profile tapped")
+    }
+    
+    //MARK: - Helpers
+    
+    func atrributedStackText(value: Int, label: String) -> NSAttributedString {
+        let attributedText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSMutableAttributedString(string: label, attributes: [.font: UIFont.systemFont(ofSize: 14),
+                                                                                    .foregroundColor: UIColor.lightGray]))
+        return attributedText
+    }
 }
