d56099e5123b133a6e81536ae4c6e1af3ed5a4cd
Taciano <maximotco@hotmail.com>
2023-01-11 14:23:23 -0300

    21. Update User After Authentication

--- InstagranClone/Controller/ProfileController.swift
+++ InstagranClone/Controller/ProfileController.swift
@@ -13,33 +13,31 @@ private let headerIdentifier = "ProfileHeader"
 class ProfileController: UICollectionViewController {
     
     //MARK: - Properties
-    var user: User? {
-        didSet {
-            collectionView.reloadData()
-        }
-    }
+    private var user: User
     
     //MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
-        fetchUser()
     }
     
     //MARK: - API
-    
-    func fetchUser() {
-        UserService.fetchUser { user in
-            self.user = user
-            self.navigationItem.title = user.username
-        }
-    }
-    
+
     //MARK: - Helpers
     
     func configureCollectionView() {
         collectionView.backgroundColor = .white
+        navigationItem.title = user.username
         collectionView.register(ProfileCell.self,
                                 forCellWithReuseIdentifier: cellIdentifier)
         collectionView.register(ProfileHeader.self,
@@ -67,13 +65,11 @@ extension ProfileController {
     
     override func collectionView(_ collectionView: UICollectionView,
                                  viewForSupplementaryElementOfKind kind: String,
-                                 at indexPath: IndexPath) -> UICollectionReusableView {        
+                                 at indexPath: IndexPath) -> UICollectionReusableView {
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
                                                                      withReuseIdentifier: headerIdentifier,
                                                                      for: indexPath) as! ProfileHeader
-        if let user = user {
-            header.viewModel = ProfileHeaderViewModel(user: user)
-        }
+        header.viewModel = ProfileHeaderViewModel(user: user)
         
         return header
     }
