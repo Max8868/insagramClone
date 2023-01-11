9837d5836efc74ea49f7db24815bf26cee241be1
Taciano <maximotco@hotmail.com>
2023-01-11 15:05:48 -0300

    22. Search Controller UI

--- InstagranClone/Controller/SearchController.swift
+++ InstagranClone/Controller/SearchController.swift
@@ -7,16 +7,37 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reusaIdentifier = "UserCell"
+
+class SearchController: UITableViewController {
     
     //MARK: - Properties
-    
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemCyan
+        configureTableView()
     }
     
     //MARK: - Helpers
+    
+    func configureTableView() {
+        view.backgroundColor = .white
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reusaIdentifier)
+        tableView.rowHeight = 64
+        
+    }
+}
+
+//MARK: - TavleViewDataSource
+extension SearchController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reusaIdentifier, for: indexPath)
+        return cell
+    }
 }
